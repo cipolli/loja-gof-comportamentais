43fc7502d190c3cb6008b9f8043fb2a583d28c76
Vinicius Cipolli - CPL <viniciuscipolli@gmail.com>
2022-04-24 21:27:37 -0300

    minha implementação de strategy usando um metodo abstrato  na classe de enum TipoImposto

--- src/br/com/cpl/imposto/TipoImposto.java
+++ src/br/com/cpl/imposto/TipoImposto.java
@@ -1,6 +1,20 @@
 package br.com.cpl.imposto;
 
+import java.math.BigDecimal;
+
 public enum TipoImposto {
-	ICMS,
-	ISS;
+	ICMS {
+		@Override
+		public BigDecimal alicotaImposto() {
+			return BigDecimal.valueOf(0.1);
+		}
+	},
+	ISS {
+		@Override
+		public BigDecimal alicotaImposto() {
+			return BigDecimal.valueOf(0.06);
+		}
+	};
+	
+	public abstract BigDecimal alicotaImposto();
 }
